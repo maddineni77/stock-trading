f75debe5efed409cbd4d6a8344fdc2d534952ca2
Maddineni Venkatarao <maddinenivenkatrao1@gmail.com>
2025-09-08 17:15:11 +0530

    Update server.js

--- stock-trading-backend/server.js
+++ stock-trading-backend/server.js
@@ -23,7 +23,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'http://localhost:5175',
-  'https://stock-trading-2.onrender.com/',
+  'https://stock-trading-2.onrender.com',
 
 ];
 const corsOptions = {
